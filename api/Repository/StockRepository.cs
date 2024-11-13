510899beb255ddc1c3f34d683d7195de6aca5747
fakeblackspectre <fakeblackspectre@protonmail.com>
2024-11-13 11:01:35 +0000

    End of section 20

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -41,8 +41,16 @@ public class StockRepository : IStockRepository
       stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
     if (!string.IsNullOrWhiteSpace(query.Symbol))
       stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+    if (!string.IsNullOrWhiteSpace(query.SortBy))
+    {
+      if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+      {
+        stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+      }
+    }
+    var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-    return await stocks.ToListAsync();
+    return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
   }
 
   public async Task<Stock?> GetByIdAsync(int id)
