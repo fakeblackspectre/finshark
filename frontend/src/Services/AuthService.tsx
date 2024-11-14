691a1b2fbe631030237bf31fe3ee293e3da45bec
fakeblackspectre <fakeblackspectre@protonmail.com>
2024-11-14 15:06:02 +0000

    End of section 34

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -4,7 +4,7 @@ import { UserProfileToken } from "../Models/User";
 
 const api = "http://localhost:5167/api";
 
-export const loginApi = async (username: string, password: string) => {
+export const loginAPI = async (username: string, password: string) => {
   try {
     const data = await axios.post<UserProfileToken>(api + "account/login", {
       username: username,
@@ -16,7 +16,7 @@ export const loginApi = async (username: string, password: string) => {
   }
 };
 
-export const registerApi = async (
+export const registerAPI = async (
   email: string,
   username: string,
   password: string,
