b3fc7efc51649544ac462bd8822f29e7792124c6
fakeblackspectre <fakeblackspectre@protonmail.com>
2024-11-14 10:48:56 +0000

    End of section 28

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -9,6 +9,7 @@ public interface IStockRepository
 {
   Task<List<Stock>> GetAllAsync(QueryObject query);
   Task<Stock?> GetByIdAsync(int id);
+  Task<Stock?> GetBySymbolAsync(string symbol);
   Task<Stock> CreateAsync(Stock stockModel);
   Task<Stock?> UpdateAsync(int id, UpdateStockRequestDTO stockDTO);
   Task<Stock?> DeleteAsync(int id);
